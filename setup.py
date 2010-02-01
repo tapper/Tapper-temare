509ae97ff5d5ddb5ed29d19f92d55dfc87a7b136
Maik Hentsche <maik.hentsche@amd.com>
2010-02-01 08:42:57 +0100

    temare: added setup file for deployment

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from decimal import Decimal
+import os
+import sys
+
+sys.path.insert(0, './src')
+
+from version import __version__
+
+setup(
+    name = "temare",
+    version = str(__version__),
+    author = "OSRC Sysint team",
+    author_email = "osrc-sysint@elbe.amd.com",
+    package_dir = {"" : "src/" },
+    url = "http://osrc/",
+    scripts = [ "temare" ],
+)
+
+import tarfile
+
+def increment_version():
+    return str(Decimal(str(__version__)) + Decimal("0.000001"))
+
+def write_version(version):
+    fh =  open('src/version.py', "w")
+    fh.write("__version__ = %s\n" % (version,))
+    fh.close()
+
+if "sdist" in sys.argv or "bdist" in sys.argv:
+    if os.environ.has_key("INCREMENT_VERSION") and \
+       os.environ["INCREMENT_VERSION"] == "1":
+        __version__ = increment_version()
+        write_version(__version__)
