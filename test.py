f4482a94bb9c3086002653b85ba25c1d3a51f465
Maik Hentsche <maik.hentsche@amd.com>
2010-02-08 14:52:09 +0100

    temare: added a first test that only checks syntactical errors

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from src import preparation
+
+import unittest
+
+class TestPreparation(unittest.TestCase):
+
+    def test_kvmhostpreparation(self):
+        prep = preparation.SubjectPreparation('bullock')
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
