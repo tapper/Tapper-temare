3bd832b5718a1b7864bae3a6d383d62d8382dd57
Maik Hentsche <maik.hentsche@amd.com>
2010-03-11 16:15:09 +0100

    temare: [TEST] test more parts of returned precondition

--- test_windows.py
+++ test_windows.py
@@ -18,6 +18,8 @@ class TestPreparation(unittest.TestCase):
     def test_xenhostpreparation_deeply(self):
         prep = preparation.SubjectPreparation('bullock')
         precondition = prep.gen_precondition_xen()
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(precondition)
         self.assertTrue(precondition['host']['root']['partition'] == '/dev/sda2')
         self.assertTrue(precondition['host']['preconditions'][0]['filename'] == \
                             't/misc_files/builds/x86_64/xen-3.4-testing/xen-3.4-testing.2010-03-02.20993_4554b305228a_.x86_64.tgz')
@@ -25,8 +27,9 @@ class TestPreparation(unittest.TestCase):
                             'suse/suse_sles10_sp2_64b_smp_raw.tar.gz')
         self.assertTrue(precondition['guests'][0]['root']['arch'] == 'windows')
         self.assertTrue(precondition['guests'][0]['root']['mounttype'] == 'windows')
-
-
+        self.assertTrue(precondition['guests'][0]['root']['mountfile'].startswith('/xen/images/001-bullock-'))
+        self.assertTrue(precondition['guests'][0]['root']['mountpartition'] == 'p1')
+#        prep.write_precondition(precondition)
 
     def test_kvmhostpreparation(self):
         prep = preparation.SubjectPreparation('bullock', 'kvm', 1)
@@ -41,6 +44,8 @@ class TestPreparation(unittest.TestCase):
         # pp.pprint(precondition)
         self.assertTrue(precondition['guests'][0]['root']['arch'] == 'windows')
         self.assertTrue(precondition['guests'][0]['root']['mounttype'] == 'windows')
+        self.assertTrue(precondition['guests'][0]['root']['mountfile'].startswith('/kvm/images/001-bullock-'))
+        self.assertTrue(precondition['guests'][0]['root']['mountpartition'] == 'p1')
         self.assertTrue(precondition['host']['testprogram']['execname'] == \
                             '/opt/artemis/bin/metainfo')
         self.assertTrue(precondition['guests'][0]['config'].has_key('exec'))
